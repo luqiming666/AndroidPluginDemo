3104096f9b8e19fb0c4eef9d6a7364f430078073
Qiming <LuQiMing@aactechnologies.com>
2023-06-14 10:08:26 +0800

    解决高版本Android上getPackageArchiveInfo()返回null的问题

--- app/src/main/java/com/beijing/zzu/plugindemo/MainActivity.java
+++ app/src/main/java/com/beijing/zzu/plugindemo/MainActivity.java
@@ -17,11 +17,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void loadPlugin(View view){
-          ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},100);
+    public void loadPlugin(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+        } else {
+            _loadPlugin();
+            startPlugin(null);
+        }
     }
 
-    public void startPlugin(View view){
+    /***
+     编译pluginapk工程生成pluginapk-debug.apk之后，执行adb push命令将.apk推到手机上
+     adb push D:\Dev\GitHub\AndroidPluginDemo\pluginapk\build\outputs\apk\debug\pluginapk-debug.apk /sdcard/Download
+
+     注意：在Android高版本上不允许直接访问共享/外部存储设备，如/sdcard，或/sdcard/Download
+     https://blog.csdn.net/xiaowang_lj/article/details/128081642
+     */
+    private void _loadPlugin() {
+        PluginManager.getInstance().setContext(this);
+        PluginManager.getInstance().loadApk(getExternalFilesDir(null).getAbsolutePath() + "/pluginapk-debug.apk");
+    }
+
+    public void startPlugin(View view) {
         if (!PluginManager.getInstance().isApkLoaded()) return;
 
         Intent intent = new Intent(this, ProxyActivity.class);
@@ -30,17 +48,13 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    // 编译pluginapk工程生成pluginapk-debug.apk之后，执行adb push命令将.apk推到手机上
-    //  adb push D:\Dev\GitHub\AndroidPluginDemo\pluginapk\build\outputs\apk\debug\pluginapk-debug.apk /sdcard
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 100) {
             if (grantResults.length > 0 &&
                     grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                PluginManager.getInstance().setContext(this);
-                PluginManager.getInstance().loadApk(Environment.getExternalStorageDirectory().getAbsolutePath() + "/pluginapk-debug.apk");
-
+                _loadPlugin();
                 startPlugin(null);
             }
         }
